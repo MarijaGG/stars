1f8a7ab2c5185b078268d06c432c178b078d4e4d
ipa23.m.gutane <ipa23.m.gutane@pjvt.local>
2024-11-20 13:49:23 +0200

    starts no sql

---
+++ migrations.sql
@@ -0,0 +1,30 @@
+-- CREATE DATABASE stars;
+
+-- USE stars;
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(50),
+	pass VARCHAR(50)
+);
+
+
+INSERT INTO users 
+(username, pass)
+VALUES 
+("Marija", "marija");
+
+SELECT * FROM users;
+
+CREATE TABLE stars (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userid INT,
+	image VARCHAR(255)
+);
+
+INSERT INTO stars 
+(userid, image)
+VALUES 
+("1", "stars2.png");
+
+SELECT * FROM stars;
+

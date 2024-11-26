3c65c33cdb683f79b1d4ebac82514223d8befd87
ipa23.m.gutane <ipa23.m.gutane@pjvt.local>
2024-11-26 10:27:54 +0200

    addod daatus db

--- index.php
+++ index.php
@@ -9,11 +9,44 @@ $data = $results->fetch_assoc();
 var_dump($data);
 
 
-echo "<form>";
-    echo "username: <input name='username'> <br>";
-    echo "password: <input name='password' type='password'> <br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $pass = $_POST["password"]; 
+
+    $sql = "INSERT INTO users 
+    (username, pass)
+    VALUES 
+    ('$username', '$pass')";
+    $conn->query($sql); }
+
+
+
+
+echo "<form method='POST'>";
+    echo "<label>username:<input name='username'> </label>  <br>";
+    echo "<label>password:<input name='password' type='password'> </label>  <br>";
     echo "<input type='submit' value='Register'>";
 echo "</form>";
+
+echo "<br>";
+
+
+
+
+
+
+
+
+
+
+$masivs = [6,8,10];
+print_r($masivs); // parasts masiivs
+echo "<br>";
+$assoc_masivs = ["vecums"=> 6,"videja_atzime"=> 8,"ids"=> 10];
+print_r($assoc_masivs); // asocioatiivais masiivs
+
+echo "<br>";
+
 
 
 
